914cbefa44109d5258054188082ab960e43c4cb2
Tarcio Almeida <tarcioalmeidalima@gmail.com>
2021-05-13 19:03:51 -0300

    login_via_senha

---
+++ src/main/java/br/com/zupacademy/tarcio/proposta/configuracao/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package br.com.zupacademy.tarcio.proposta.configuracao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_nossocartao")
+                                .antMatchers(HttpMethod.POST, "/propostas").hasAuthority("SCOPE_nossocartao")
+                                
+                                .antMatchers(HttpMethod.POST, "/biometrias").hasAuthority("SCOPE_nossocartao")
+                                
+                                .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_nossocartao")
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
